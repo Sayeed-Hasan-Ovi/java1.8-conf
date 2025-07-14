ddac6e48f3c0df20c5d4ae00131159fbb9722432
MD. SAYEED HASAN OVI <1805065@ugrad.cse.buet.ac.bd>
2025-07-14 12:17:37 +0000

    ./gradlew clean build

---
+++ Aml360Application.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Aml360Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Aml360Application.class, args);
+	}
+
+}
